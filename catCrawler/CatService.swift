119f147b354ae4e04cf6a7ad4f4c71c33903cf37
syss220211 <syss220211@gmail.com>
2022-11-22 01:37:28 +0900

    api test 전까지

--- catCrawler/CatService.swift
+++ catCrawler/CatService.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+
+final class CatService {
+    
+    enum RequestError: Error {
+        case networkError
+    }
+    
+    // 페이지와 리미트 값을 받는 함수
+    func getCats(
+        page: Int,
+        limit: Int,
+        completion: @escaping (Result<String, RequestError>) -> Void
+        // Result : 성공 실패 여부를 담을 수 있는 스위프트 변수
+    ) {
+        var components = URLComponents(string: "http://api.thecatapi.com/v1/images/search")!
+        
+        components.queryItems = [
+            URLQueryItem(name: "page", value: "\(page)"),
+            URLQueryItem(name: "page", value: "\(page)")
+        ]
+        
+        var request = URLRequest(url: components.url!)
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) {
+            data, response, error in
+            
+            guard error != nil else {
+                completion(.failure(.networkError))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.networkError))
+                return
+            }
+            guard let response = String(data: data, encoding: .unicode)
+            else {
+                completion(.failure(.networkError))
+                return
+            }
+            print(response)
+            
+            completion(.success(response))
+    
+        }
+    }
+}
